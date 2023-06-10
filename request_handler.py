af8bb632b28a048942caae76e8687b96d1cf2106
Pritesh Zaver <priteshzaver@gmail.com>
2023-06-10 16:02:20 -0500

    book 1 chapter 2 complete

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,8 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from views import get_all_animals
+
 
 
 # Here's a class. It inherits from another class.
@@ -27,13 +30,7 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         # It's an if..else statement
         if self.path == "/animals":
-            # In Python, this is a list of dictionaries
-            # In JavaScript, you would call it an array of objects
-            response = [
-                {"id": 1, "name": "Snickers", "species": "Dog"},
-                {"id": 2, "name": "Lenny", "species": "Cat"}
-            ]
-
+            response = get_all_animals()
         else:
             response = []
 
